4bf64fc9bae535a51ba9f8265bbab8e916b49651
Andy46 <Andy46@users.noreply.github.com>
2024-12-03 07:39:38 +0100

    Day 3 part 2

--- 2024/03_MullItOver/main.py
+++ 2024/03_MullItOver/main.py
@@ -1,19 +1,29 @@
 import re
 
+filename="example.data"
 filename="test.data"
+
 muls=[]
 with open(filename, "r") as file:
     data=file.read()
-    muls=re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", data)
+    muls=re.findall(r"(do\(\))|(mul\(\d{1,3},\d{1,3}\))|(don\'t\(\))", data)
 
-# Part 1
+# Part 2
 total=0
+enable=True
 for mul in muls:
     print (mul)
-    a = int(mul[0])
-    b = int(mul[1])
-    total = total + (a*b)
+    if 'do()' in mul:
+        enable = True
+    elif r"don't()" in mul:
+        enable = False
+    elif enable == True:
+        elems = re.findall(r"(\d{1,3}),(\d{1,3})", mul[1])[0]
+        print (elems)
+        a = int(elems[0])
+        b = int(elems[1])
+        print(a)
+        print(b)
+        total = total + (a*b)
 
-print("Total: " + str(total))
-    
-
+print("Total 2: " + str(total))
