e39c9337dc89136d9133d8d9f71b84af19e36253
Andy46 <andymanu1992@gmail.com>
2024-12-12 10:28:33 +0100

    Calculate external sides

--- 2024/12_GardenGroups/main.py
+++ 2024/12_GardenGroups/main.py
@@ -6,16 +6,21 @@ from multiprocessing import Pool
 # Files
 import sys, os
 filepath = os.path.dirname(sys.argv[0])
-filename = f"{filepath}/00_example2.data"
-filename = f"{filepath}/00_example3.data"
-filename = f"{filepath}/00_test.data"
 
 filename = f"{filepath}/00_example1.data"
+filename = f"{filepath}/00_example2.data"
+filename = f"{filepath}/00_example3.data"
+filename = f"{filepath}/00_example4.data"
+filename = f"{filepath}/00_example5.data"
+
+filename = f"{filepath}/00_test.data"
 
 # Read data
 initialData = []
 with open(filename, "r") as file:
     initialData = list(list(line.strip()) for line in file.readlines())
+
+initialGarden = copy.deepcopy(initialData)
 
 #######################################
 # Part 1 -  #
@@ -25,15 +30,14 @@ def printGarden(garden):
         print(row)
 
 # TODO: Change is in garden to use local variable
-# def isInGarden(x, y):
-#     return ((x >= 0) and (x < len(initialData))) and ((y >= 0) and (y < len(initialData[0])))
 def isInGarden(cell):
     return ((cell[0] >= 0) and (cell[0] < len(initialData))) and ((cell[1] >= 0) and (cell[1] < len(initialData[0])))
 
+def getContiguousCells (x, y):
+    return [[x-1,y],[x,y-1],[x+1,y],[x,y+1]]
+
 def getContiguousPlant(x, y):
-    cells = [[x-1,y],[x,y-1],[x+1,y],[x,y+1]]
-    cells = list(filter(isInGarden, cells))
-    return cells
+    return list(filter(isInGarden, getContiguousCells(x, y)))
 
 def clearCell(garden, x, y):
     garden[x][y] = None
@@ -51,7 +55,8 @@ def getRegion(garden, x, y):
         nextCells = []
         for cell in newCells:
             contiguousCells = getContiguousPlant(cell[0], cell[1])
-            nextCells = [c for c in contiguousCells if garden[c[0]][c[1]] == plant]
+            nextCells.extend([c for c in contiguousCells if (garden[c[0]][c[1]] == plant)])
+            [clearCell(garden, c[0], c[1]) for c in nextCells]
         newCells = nextCells
 
     return plant, region
@@ -62,38 +67,109 @@ def findRegions(garden):
         for y in range(len(garden[0])):
             if garden[x][y] is not None:
                 plant, region = getRegion(garden, x, y)
-                print(f"{plant}: {region}")
                 if plant in regions:
                     regions[plant].append(region)
                 else:
                     regions[plant] = [region]
     return regions
 
+def calculateFence(garden, plant, region):
+    fences = 0
+    for cell in region:
+        contiguous = getContiguousCells(cell[0], cell[1])
+        for other in contiguous:
+            if not isInGarden(other):
+                fences += 1 
+            elif garden[other[0]][other[1]] != plant:
+                fences += 1
+    return fences
 
-# print(f"=== Part 0 ===")
-# gardenCopy = copy.deepcopy(initialData)
-# printGarden(gardenCopy)
-# print(f"Region for {[0,0]}: {getRegion(gardenCopy, 0, 0)}")
-# printGarden(gardenCopy)
-# print(f"==============")
-
-# Calculate
-gardenCopy = copy.deepcopy(initialData)
-regions = findRegions(gardenCopy)
+def calculateFencePrice(garden, regions):
+    totalPrice = 0
+    for plant in regions.keys():
+        for region in regions[plant]:
+            regionFences = calculateFence(garden, plant, region)
+            totalPrice += (regionFences * len(region))
+    return totalPrice
 
 # Output results
 
 print(f"=== Part 1 ===")
-for key in regions.keys():
-    print(f"{key}: {regions[key]}")
 
-allRegions = []
-for plantRegions in regions.values():
-    print(plantRegions)
-    allRegions.extend(plantRegions)
-print(allRegions)
+# Find regions
+gardenCopy = copy.deepcopy(initialData)
+regions = findRegions(gardenCopy)
+
+# Calculate total fence price
+gardenCopy = copy.deepcopy(initialData)
+fencePrice = calculateFencePrice(gardenCopy, regions)
+
+print(f"Fences price: {fencePrice}")
 print(f"==============")
 
+
 print(f"=== Part 2 ===")
+
+def calculateExternalSides(region):
+    def rotate(token, rotation):
+        # CW  -> dir = 1
+        # CCW -> dir = -1
+        dirs = ['n', 'e', 's', 'w']
+
+        return dirs[((dirs.index(token) + rotation) + len(dirs)) % len(dirs)]
+    
+    def getNextCell(x, y, dirToken):
+        if dirToken == 'n':
+            return x-1, y
+        elif dirToken == 'e':
+            return x, y+1
+        elif dirToken == 's':
+            return x+1, y
+        elif dirToken == 'w':
+            return x, y-1
+
+    firstCell = region[0]
+    firstDir  = 'e'
+    
+    currentCell = firstCell
+    currentDir = firstDir
+
+    sides = 0
+    
+    region = list(map(tuple, region))
+    while True:
+        nextLeftCell  = getNextCell(currentCell[0], currentCell[1], rotate(currentDir, -1))
+        nextFrontCell = getNextCell(currentCell[0], currentCell[1], currentDir)
+
+        if tuple(nextLeftCell) in region:
+            currentDir = rotate(currentDir, -1)
+            currentCell = nextLeftCell
+            sides = sides + 1
+
+        elif tuple(nextFrontCell) in region:
+            currentDir = currentDir
+            currentCell = nextFrontCell
+
+        else:
+            currentDir = rotate(currentDir, 1)
+            currentCell = currentCell
+            sides = sides + 1
+
+        if (tuple(currentCell) == tuple(firstCell)) and (currentDir == firstDir):
+            break
+    return sides
+
+def calculateSidesPrice(regions):
+    totalPrice = 0
+    for plant in regions.keys():
+        for region in regions[plant]:
+            regionSides = calculateExternalSides(region)
+            # print(f"{plant}({regionSides}): {region}")
+            totalPrice += (regionSides * len(region))
+    return totalPrice
+
+sidePrice = calculateSidesPrice(regions)
+print(f"Sides price: {sidePrice}")
+
 print(f"==============")
 
