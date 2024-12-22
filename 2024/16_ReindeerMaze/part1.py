0b9a63e97eacbe182c219e368b4a0e44ff9a3d30
Andy46 <agambmel@gmail.com>
2024-12-22 17:48:52 +0100

    Solve part 1

--- 2024/16_ReindeerMaze/part1.py
+++ 2024/16_ReindeerMaze/part1.py
@@ -7,10 +7,10 @@ import click
 import sys, os
 filepath = os.path.dirname(sys.argv[0])
 
-filename = f"{filepath}/00_example2.data"
-filename = f"{filepath}/00_test.data"
-
 filename = f"{filepath}/00_example1.data"
+filename = f"{filepath}/00_example2.data"
+
+filename = f"{filepath}/00_test.data"
 
 # Read data
 INITIAL_MAZE = []
@@ -20,13 +20,12 @@ with open(filename, "r") as file:
 
 # Helper functions
 def printMaze(maze):
-    N = 3
     for row in maze:
         for item in row:
             if str(item) in "E#.S":
-                print (item*N, end='')
+                print (item*6, end='')
             else:
-                print("{0:3d}".format(item), end='')
+                print("{0:6d}".format(item), end='')
         print("")
 
 # Find and detection functions
@@ -38,13 +37,22 @@ def findToken(maze, token):
     print(f"ERROR: No {token} found")
     return None
 
+
 def findEnd(maze):
     TOKEN_END   = 'E'
     return findToken(maze, TOKEN_END)
 
+def isEnd(maze, pos):
+    TOKEN_END   = 'E'
+    return maze[pos[1]][pos[0]] == TOKEN_END
+
 def findStart(maze):
     TOKEN_START   = 'S'
     return findToken(maze, TOKEN_START)
+
+def isStart(maze, pos):
+    TOKEN_START   = 'S'
+    return maze[pos[1]][pos[0]] == TOKEN_START
 
 def isWall(maze, pos):
     TOKEN_WALL  = '#'
@@ -56,45 +64,80 @@ def isEmpty(maze, pos):
 
 # Transformation functions
 
-def getAllDirPositions(reindeer):
-    return {'n' : [reindeer[0]  , reindeer[1]-1],
-            'e' : [reindeer[0]+1, reindeer[1]  ],
-            's' : [reindeer[0]  , reindeer[1]+1],
-            'w' : [reindeer[0]-1, reindeer[1]  ]}
+def getAllDirPositions(position):
+    return [[[position[0]  , position[1]-1], 'n'],
+            [[position[0]+1, position[1]  ], 'e'],
+            [[position[0]  , position[1]+1], 's'],
+            [[position[0]-1, position[1]  ], 'w']]
 
-def getDirPosition(reindeer, direction): 
-    return getAllDirPositions([direction])
+# def getDirPosition(reindeer, direction): 
+#     return getAllDirPositions([direction])
 
-def rotate(direction, rotation): # CW=1, CCW=-1
-    directions = ['n', 'e', 's', 'w']
-    dir = (direction + len(directions) + rotation) % len(directions)
-    return dir
+# def rotate(direction, rotation): # CW=1, CCW=-1
+#     directions = ['n', 'e', 's', 'w']
+#     dir = (direction + len(directions) + rotation) % len(directions)
+#     return dir
 
 # Maze related functions
-def isValidPosition(maze, position):
-    return isEmpty(maze, position)
-
-def getNextPositionsInMaze(maze, reindeer):
-    allPositions = list(getAllDirPositions(reindeer).values())
-    validPositions = [position for position in allPositions if isValidPosition(maze, position)]
-    return validPositions
+# def isValidPosition(maze, position):
+#     return isEmpty(maze, position)
 
 def setMazeCell(maze, cell, cost):
     maze[cell[1]][cell[0]] = cost
 
+def getMazeCell(maze, cell):
+    return maze[cell[1]][cell[0]]
+
+def getNextPositionsInMaze(maze, reindeer, LAST):
+    if all([a==b for a,b in zip(reindeer[0], LAST)]):
+        return []
+    
+    currentPosition  = reindeer[0]
+    currentDirection = reindeer[1]
+    currentCost      = reindeer[2]
+
+    directions = ['n','e','s','w']
+    allNextPositions = getAllDirPositions(currentPosition)
+
+    nextPositions = []
+    for newPosition, newDirection in allNextPositions:
+        ROTATION_COST = 1000
+        FORWARD_COST  = 1
+
+        rotationCW = abs(directions.index(newDirection) - directions.index(currentDirection))
+        rotationCCW = 4 - rotationCW
+        rotations = min([rotationCW,rotationCCW])
+
+        newCost = currentCost + ((rotations * ROTATION_COST) + FORWARD_COST)
+        
+        # Write cost to cell
+        if not isWall(maze, newPosition):
+            if isEnd(maze, newPosition) or isEmpty(maze, newPosition):
+                setMazeCell(maze, newPosition, newCost)
+                nextPositions.append([newPosition, newDirection, newCost])
+            elif isStart(maze, newPosition):
+                continue
+            else:
+                cellCost = getMazeCell(maze, newPosition)
+                if newCost < cellCost:
+                    setMazeCell(maze, newPosition, newCost)
+                    nextPositions.append([newPosition, newDirection, newCost])
+
+    return nextPositions
+
+
 def floodFill(MAZE, FIRST, LAST):
     maze = copy.deepcopy(MAZE)
 
-    newCells = [FIRST]
+    newPositions = [[FIRST, 'e', 0]]
     cost = 0
-    while len(newCells):
-        nextCells = [] 
-        for cell in newCells:
-            setMazeCell(maze, cell, cost)
-            nextCells.extend(getNextPositionsInMaze(maze, cell))
+    while len(newPositions):
+        nextPositions = [] 
+        for cell in newPositions:
+            nextPositions.extend(getNextPositionsInMaze(maze, cell, LAST))
         cost += 1
         # printMaze(maze)
-        newCells = nextCells
+        newPositions = nextPositions
     return maze
 
 # ###################################
@@ -114,9 +157,9 @@ if __name__ == "__main__":
     print(f"Start: {START} - End: {END}")
 
     # Run part 1
-    filledMaze = floodFill(maze, END, START)
-    print(f"\nFinal maze:")
-    printMaze(filledMaze)
+    # filledMaze = floodFill(maze, END, START)
+    # print(f"\nFinal maze:")
+    # printMaze(filledMaze)
 
     filledMaze = floodFill(maze, START, END)
     print(f"\nFinal maze:")
@@ -126,5 +169,5 @@ if __name__ == "__main__":
     # gpsTotal = calculateGPS(warehouse)
 
     # Output result
-    lowestScore = 0
+    lowestScore = getMazeCell(filledMaze, END)
     print(f"Lowest score: {lowestScore}")
