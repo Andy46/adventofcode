a29d2cc1ced40079603b929d829712aff22fbb2d
Andy46 <agambmel@gmail.com>
2025-12-04 19:25:17 +0100

    Solve day 01/12/2025 (Part 1)

---
+++ 2025/01_SecretEntrance/main.py
@@ -0,0 +1,25 @@
+import hashlib
+
+# Files
+import sys, os
+filepath = os.path.dirname(sys.argv[0])
+filename = f"{filepath}/example1.data"
+filename = f"{filepath}/test1.data"
+
+INITIAL_POS = 50
+
+password = 0
+currentPos = INITIAL_POS
+print(f"Initial position: {currentPos}")
+with open(filename, "r") as file:
+    for line in [line.strip() for line in file.readlines()]:
+        op = line[0]
+        delta = int(line[1:])
+        currentPos = (currentPos - delta) if op == 'R' else (currentPos + delta)
+        print(f"Current position: {currentPos}")
+        if currentPos % 100 == 0:
+            password = (password + 1)
+
+print(f"The password is: {password}")
+
+    
