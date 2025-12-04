2c98192ebc00bf83e6797eb2696c5c88d2689d3b
Andy46 <agambmel@gmail.com>
2025-12-04 20:42:24 +0100

    Improve day 01/12/2025 a bit

--- 2025/01_SecretEntrance/Part2/main.py
+++ 2025/01_SecretEntrance/Part2/main.py
@@ -4,7 +4,7 @@ filepath = os.path.dirname(sys.argv[0])
 filename = f"{filepath}/example.data"
 filename = f"{filepath}/test.data"
 
-INITIAL_POS = 50
+INITIAL_POS = 100050
 
 password = 0
 currentPos = INITIAL_POS
@@ -14,16 +14,25 @@ with open(filename, "r") as file:
         op = line[0]
         delta = int(line[1:])
 
-        for i in range(0, delta):
-            currentPos = (currentPos + 1) if op == 'R' else (currentPos - 1)
-            if currentPos < 0:
-                currentPos = currentPos + 100
-            elif currentPos >= 100:
-                currentPos = currentPos - 100
-            #print(f"Current position: {currentPos}")
-            if currentPos == 0:
-                password = password + 1
-                #print(f"Password: {password}")
+        completeLoops = int(abs(delta)/100)
+        password = password + completeLoops
+
+        delta = delta % 100
+        newPos = 0
+        if op == 'R':
+            newPos = (currentPos + delta) 
+        else:
+            newPos = (currentPos - delta)
+            if abs(newPos) % 100 == 0:
+                password = (password + 1)
+
+        newPosInt = int(newPos/100)
+        currentPosInt = int(currentPos/100)
+        if  currentPos % 100 != 0 and newPosInt != currentPosInt:
+            password = password + 1
+
+        currentPos = newPos
+
 
 print(f"The password is: {password}")
 
