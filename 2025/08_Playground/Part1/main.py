938e889432d1c773633901327221f2dc8c8cb615
Andres Gamboa <andymanu1992@gmail.com>
2025-12-20 10:09:42 +0100

    Solve day 08/12/2025 (Part 1)

---
+++ 2025/08_Playground/Part1/main.py
@@ -0,0 +1,112 @@
+# Files
+import sys, os
+import math
+filepath = os.path.dirname(sys.argv[0])
+filename = f"{filepath}/example.data"
+filename = f"{filepath}/test.data"
+
+# Step 1 Calculate distances
+# Step 2 Get smallest distances per box pair
+# Step 3 Order pairs by smallest distances
+# Step 4 Connect non connected boxes
+
+def readData(filename : str) -> list[tuple]:
+    with open(filename, "r") as file:
+        text = [line.strip() for line in file.readlines()]
+        boxes = [tuple(map(int, item.split(','))) for item in text]
+        # items = [item.split(',') for item in text]
+        # castMatrix(boxes, int)
+        return boxes
+    
+def printData(data : list[any]) -> None:
+    for item in data:
+        print(item)
+
+# AUX FUNCTIONS
+def calcDistance3D(boxA, boxB):
+    # distX=abs(boxB[0]-boxA[0])
+    # distY=abs(boxB[1]-boxA[1])
+    # distZ=abs(boxB[2]-boxA[2])
+    # dist3D=math.sqrt((distX*distX)+(distY*distY)+(distZ*distZ))
+    dist3D = math.sqrt(sum([abs((a-b)*(a-b)) for a,b in zip(boxA, boxB)]))
+    return dist3D
+
+class Distance:
+    def __init__(self, boxA, boxB):
+        self.boxA = min(boxA, boxB)
+        self.boxB = max(boxA, boxB)
+        self.dist = calcDistance3D(self.boxA, self.boxB)
+
+    def __hash__(self):
+        return (self.boxA, self.boxB, self.dist).__hash__()
+    
+    def __str__(self):
+        return f"{self.boxA} - {self.boxB} : {self.dist}"
+
+def calcAllDistances(boxes : list[tuple]) -> list[list[float]]:
+    distances = set()
+    for a, boxA in enumerate(boxes):
+        for b, boxB in enumerate(boxes[a+1:]):
+            distances.add(Distance(boxA, boxB))
+
+    sortedDistances = sorted(distances, key=lambda x: x.dist)
+
+    return sortedDistances
+
+def sortDistances(distances):
+    return sorted(distances, key=lambda x:x.getDistance())
+
+def makeConnections(boxes, distances):
+    circuits = []
+    connectionCount = 1000
+    for i in range(connectionCount):
+        inserted = False
+        dist = distances[i]
+        for circuit in circuits:
+            if dist.boxA in circuit or dist.boxB in circuit:
+                circuit.add(dist.boxA)
+                circuit.add(dist.boxB)
+                inserted = True
+                break
+        if not inserted:
+            newCircuit = set()
+            newCircuit.add(dist.boxA)
+            newCircuit.add(dist.boxB)
+            circuits.append(newCircuit)
+    return circuits
+
+def reduceCircuits(oldCircuits):
+    newCircuits = []
+    count = 0
+    for oldCircuit in oldCircuits:
+        inserted = False
+        for newCircuit in newCircuits:
+            if any([box in newCircuit for box in oldCircuit]):
+                newCircuit.update(oldCircuit)
+                inserted = True
+                count = count + 1
+                break
+        if not inserted:
+            newCircuits.append(oldCircuit)
+    return newCircuits, count
+
+# PUZZLE SOLVING
+if __name__=="__main__":
+    boxes = readData(filename)
+    # printData(boxes)
+    distances = calcAllDistances(boxes)
+    # printData(distances)
+    circuits = makeConnections(boxes, distances)
+    # printData(circuits)
+    circuits, c = reduceCircuits(circuits)
+    while c != 0:
+        circuits, c = reduceCircuits(circuits)
+    # printData(circuits)
+    orderedCircuits = sorted(circuits, key=lambda x:len(x), reverse=True)
+    # printData(orderedCircuits)
+
+    import math
+    total = math.prod([len(circuit) for circuit in orderedCircuits[:3]])
+
+    print(f"Total circuit operations: {total}")  
+    
